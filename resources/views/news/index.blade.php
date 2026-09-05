bbab8726992d81a0d7a752dbfedbc0e60b3ae27e
Tom Ignoul <tom.ignoul@student.ehb.be>
2026-09-05 08:16:40 +0200

    Add news, controller, route and view

---
+++ resources/views/news/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('title', 'News')
+
+@section('content')
+    <h1>News</h1>
+
+    <article>
+        <h2>Club championship starts next week</h2>
+        <p>The annual championship kicks off on Wednesday. Sign up at the desk.</p>
+    </article>
+
+    <article>
+        <h2>New training evenings for beginners</h2>
+        <p>Starting in October we run a beginners session before the regular club night.</p>
+    </article>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NewsController;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
+
+Route::get('news', [\App\Http\Controllers\NewsController::class, 'index'])->name('news.index');
