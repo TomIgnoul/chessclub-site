dfaa6c31d91b6ada8a6aba17dae89c7d7f903f8b
Tom Ignoul <tom.ignoul@student.ehb.be>
2026-09-07 21:11:28 +0200

    Add more news items to seeder

--- database/seeders/NewsSeeder.php
+++ database/seeders/NewsSeeder.php
@@ -18,5 +18,16 @@ class NewsSeeder extends Seeder
             'content' => 'The annual championship kicks off on Wednesday.',
             'published_at' => now(),
         ]);
+
+        News::create([
+            'title' => 'New: Training week for beginners',
+            'content' => 'From October there will be a beginners session',
+            'published_at' => now()->subDays(3),
+        ]);
+        News::create([
+            'title' => 'A new world champion',
+            'content' => 'A clean win 6/7 score wins against the top seeds for Alex!',
+            'published_at' => now()->subWeek(),
+        ]);
     }
 }
